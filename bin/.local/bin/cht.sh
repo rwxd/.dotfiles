443360fd70c97b5967e1773c0320be5469b5c888
Wrage, Florian <florian.wrage@akquinet.de>
2021-09-15 15:47:25 +0200

    searching cht.sh seems to work

--- bin/.local/bin/cht.sh
+++ bin/.local/bin/cht.sh
@@ -3,8 +3,13 @@ read -p "Enter Query: " query
 
 query=`echo $query | tr ' ' '+'`
 
+if [ $query != "\n" ] ; then
+  echo "if successful"
+  query=$(echo "/"$query)
+fi
+
 if { [ -n "$TMUX" ]; } then
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "echo \"curl cht.sh/$selected$query/\" & curl cht.sh/$selected$query & while [ : ]; do sleep 1; done"
 else
-    curl -s cht.sh/$selected/$query | less
+    curl -s cht.sh/$selected$query | less
 fi
