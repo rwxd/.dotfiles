ec4867e76f0e1293559c55b2b1ca9d81be53bf45
Wrage, Florian <florian.wrage@akquinet.de>
2021-09-15 15:00:58 +0200

    fixed script to work in and without tmux

--- bin/.local/bin/cht.sh
+++ bin/.local/bin/cht.sh
@@ -3,7 +3,7 @@ read -p "Enter Query: " query
 
 query=`echo $query | tr ' ' '+'`
 
-if { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
+if { [ -n "$TMUX" ]; } then
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
     curl -s cht.sh/$selected/$query | less
