a94d5c560bb954f115f74de2268daec78c3aa5a0
Wrage, Florian <florian.wrage@akquinet.de>
2021-09-15 16:05:28 +0200

    fzf reverse when in tmux

--- bin/.local/bin/cht.sh
+++ bin/.local/bin/cht.sh
@@ -1,4 +1,10 @@
-selected=`curl -s https://cht.sh/:list | grep "^\w*" | sort --unique | fzf`
+if [ -n "$TMUX" ]; then
+    fzf_params="--reverse"
+else
+    fzf_params=""
+fi
+
+selected=`curl -s https://cht.sh/:list | grep "^\w*" | sort --unique | fzf $fzf_params`
 read -p "Enter Query: " query
 
 query=`echo $query | tr ' ' '+'`
