e93cf9fc423ad54a79572300dd674535ed731490
rwxd <rwxd@pm.me>
2022-08-09 13:34:05 +0200

    feat(treesitter): added context plugin

--- nvim/.config/nvim/lua/rwxd/treesitter.lua
+++ nvim/.config/nvim/lua/rwxd/treesitter.lua
@@ -1,4 +1,4 @@
-require 'nvim-treesitter.configs'.setup {
+require "nvim-treesitter.configs".setup {
 	-- A list of parser names, or "all"
 	ensure_installed = {
 		"c", "lua", "rust", "python", "go", "latex", "query",
@@ -50,3 +50,36 @@ require "nvim-treesitter.configs".setup {
 		},
 	}
 }
+
+require("treesitter-context").setup({
+	enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+	throttle = true, -- Throttles plugin updates (may improve performance)
+	max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+	show_all_context = true,
+	patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+		-- For all filetypes
+		-- Note that setting an entry here replaces all other patterns for this entry.
+		-- By setting the 'default' entry below, you can control which nodes you want to
+		-- appear in the context window.
+		default = {
+			"function",
+			"method",
+			"for",
+			"while",
+			"if",
+			"switch",
+			"case",
+		},
+
+		-- rust = {
+		-- 	"loop_expression",
+		-- 	"impl_item",
+		-- },
+		--
+		-- typescript = {
+		-- 	"class_declaration",
+		-- 	"abstract_class_declaration",
+		-- 	"else_clause",
+		-- },
+	},
+})
